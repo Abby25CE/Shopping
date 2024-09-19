7231462342caf4fbe43558d6637ec5c3238ed19f
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2024-09-19 14:01:25 -0600

    1:48:00

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,44 +1,74 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
-export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+const initialState = [];
+const reducer = (state, action) => {
+  const { type: actionType, playload: actionPlayload } = action;
+  switch (actionType) {
+    case "ADD_TO_CART": {
+      const { id } = actionPlayload;
+      const productInCartIndex = state.findIndex((item) => item.id === id);
 
-  const addToCart = (product) => {
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
-
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart);
-      newCart[productInCartIndex].quantity += 1;
-      return setCart(newCart);
-    }
-
-    setCart((prevState) => [...prevState, { ...product, quantity: 1 }]);
-  };
-
-  const removeFromCart = (product) => {
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
-
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart);
-      newCart[productInCartIndex].quantity -= 1;
-      if (newCart[productInCartIndex].quantity <= 0) {
-        newCart.splice(productInCartIndex, 1);
+      if (productInCartIndex >= 0) {
+        const newCart = structuredClone(state);
+        newCart[productInCartIndex].quantity += 1;
+        return newCart;
       }
-      return setCart(newCart);
+
+      return [
+        ...state,
+        {
+          ...actionPlayload,
+          quantity: 1,
+        },
+      ];
     }
+    case "REMOVE_FROM_CART": {
+      const { id } = actionPlayload;
+      const productInCartIndex = state.findIndex((item) => item.id === id);
 
-    setCart((prevState) => prevState.filter((item) => item.id !== product.id));
-  };
+      if (productInCartIndex >= 0) {
+        const newCart = structuredClone(state);
+        newCart[productInCartIndex].quantity -= 1;
+        if (newCart[productInCartIndex].quantity <= 0) {
+          newCart.splice(productInCartIndex, 1);
+        }
+        return newCart;
+      }
 
-  const cleanCart = () => {
-    setCart([]);
-  };
+      return state.filter((item) => item.id !== id);
+    }
+    case "CLEAN_CART": {
+      return initialState;
+    }
+  }
+
+  return state;
+};
+
+export function CartProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addToCart = (product) =>
+    dispatch({
+      type: "ADD_TO_CART",
+      playload: product,
+    });
+
+  const removeFromCart = (product) =>
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      playload: product,
+    });
+  const cleanCart = () =>
+    dispatch({
+      type: "CLEAN_CART",
+    });
 
   return (
     <CartContext.Provider
-      value={{ cart, removeFromCart, addToCart, cleanCart }}
+      value={{ cart: state, removeFromCart, addToCart, cleanCart }}
     >
       {children}
     </CartContext.Provider>
